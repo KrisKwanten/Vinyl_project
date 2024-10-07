46bade7d3b17f8769a4f3e416c8aad8d5536e387
KrisKwanten <r0996202@student.thomasmore.be>
2024-10-07 20:55:20 +0200

    klaar met les 2/10

--- resources/views/layouts/vinylshop.blade.php
+++ resources/views/layouts/vinylshop.blade.php
@@ -3,17 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <script src="https://cdn.tailwindcss.com"></script>
+    <x-layout.favicons/>
+{{--    <script src="https://cdn.tailwindcss.com"></script>--}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
     <meta name="description" content="{{ $description ?? 'Welcome to the Vinyl Shop' }}">
     <title>{{ $title ?? 'The Vinyl Shop' }}</title>
-    <x-Layout.favicons/>
 </head>
 <body class="font-sans antialiased">
 <div class="flex flex-col space-y-2 min-h-screen text-gray-800 bg-gray-100">
     <header class="shadow bg-white/70 sticky inset-0 backdrop-blur-sm z-10">
         {{--  Navigation  --}}
-        <x-layout.nav />
+        <x-layout.nav/>
     </header>
     <main class="container mx-auto flex-1 px-4">
         {{-- Title --}}
@@ -23,9 +24,9 @@
         {{-- Main content --}}
         {{ $slot }}
     </main>
-    <x-layout.footer />
-
+    <x-layout.footer/>
 </div>
 @stack('script')
+@livewireScripts
 </body>
 </html>
